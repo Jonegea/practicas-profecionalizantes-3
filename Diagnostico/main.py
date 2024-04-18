0cb29b22f04b3bde6cf103d45494543dd7b81485
Jonegea <jon_11_490@hotmail.com>
2024-04-18 16:21:39 -0300

    Cargando datos nuevamente

--- Diagnostico/main.py
+++ Diagnostico/main.py
@@ -5,7 +5,7 @@ def read_data_from_excel(file_path):
 
     wb = load_workbook(filename=file_path)
     sheet = wb.active
-
+    #utilizo row para iterar las filas
     for row in sheet.iter_rows(min_row=4, values_only=True):
         nombre = row[0]
         municipio = row[5]
@@ -15,7 +15,7 @@ def read_data_from_excel(file_path):
     
     return set_datos
 
-# Ejemplo de uso
+# cargo o leo el archivo excel
 if __name__ == "__main__":
     file_path = "Localidades.xlsx"
     datos = read_data_from_excel(file_path)

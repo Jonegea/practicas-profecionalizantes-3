e84b40e467733a8d107776ced85da2f4d4b88254
jon_11_490 <jon_11_490rhotmail.com>
2024-06-06 18:42:02 -0300

    version completa.. supongo

---
+++ calculator-v0/calculator-v1/main.js
@@ -0,0 +1,15 @@
+import { WCModel } from './WCModel.js';
+import { WCView } from './WCView.js';
+
+function main()
+{
+	let model = new WCModel();
+	let view = new WCView(model);
+
+	document.body.appendChild(view);
+
+}
+
+window.onload = main;
+
+export { main };
